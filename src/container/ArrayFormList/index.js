41f85e2ff45489ec2eed1ece7cac191675f499ad
Gayathri <b.gayathri@skava.com>
2020-02-25 18:55:26 +0530

    one

--- src/container/ArrayFormList/index.js
+++ src/container/ArrayFormList/index.js
@@ -1,11 +1,11 @@
 class ArrayFormList {
-            name = ["myName"]
-            price = ["myPrice"]
+            name = ["myName"];
+            price = ["myPrice"];
             product = [{
                 'id': 'myID',
                 'name': 'myName',
                 'price': 'myPrice',
-            },]
+            },];
 }
 const arrayFormListObj = new ArrayFormList()
 export default arrayFormListObj 
